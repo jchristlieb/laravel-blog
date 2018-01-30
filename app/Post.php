a16a426f015ff87eb749785bd93d83b80fc53bb3
Jan <mail@janchristlieb.de>
2018-01-30 16:43:19 +0100

    changed the keyRoute from id to slug for posts

--- app/Post.php
+++ app/Post.php
@@ -5,6 +5,21 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
+
 {
-    //
+
+
+    protected $fillable = ['title', 'slug', 'abstract', 'body'];
+
+    public function getRouteKeyName()
+    {
+
+        return 'slug';
+
+    }
+
+
 }
+
+
+
