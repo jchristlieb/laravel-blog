3daa9ba2ceede509e1a1b63eff808020799ffa6a
Jan <mail@janchristlieb.de>
2018-01-30 16:42:34 +0100

    added @store function to postController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,15 +11,18 @@ class PostController extends Controller
 
     {
 
-        return view('blog.index');
+        $posts = Post::latest()->get();
+
+        return view('blog.index', compact('posts'));
 
     }
 
-    public function show()
+    public function show(Post $post)
 
     {
 
-        return view('blog.show');
+        // route model binding
+        return view('blog.show', compact('post'));
 
     }
 
@@ -35,18 +38,21 @@ class PostController extends Controller
 
     {
 
+        $this->validate(request(), [
+            'title' => 'required|max: 80',
+            'abstract' => 'required|string',
+            'body' => 'required|min: 20',
+        ]);
 
         // create a new post using the request data
-
         // dd(request()->all());
+
         $post = new Post;
 
         $post->title = request('title');
         $post->slug = str_slug(request('title'));
         $post->abstract = request('abstract');
         $post->body = request('body');
-
-
 
         // save it to the database
 
