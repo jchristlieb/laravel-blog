b52362be32a510b4fd5dafc55825a9f8ff8a7d6a
Jan <mail@janchristlieb.de>
2018-01-30 16:41:32 +0100

    added two links to nav bar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -8,10 +8,10 @@
     <div class="collapse navbar-collapse" id="navbarsExample03">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="/blog">Blog<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="/blog/create">Create Post</a>
             </li>
         </ul>
         <ul class="navbar-nav ml-auto">
