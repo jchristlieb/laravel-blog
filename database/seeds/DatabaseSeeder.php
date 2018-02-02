040b67363d239f578081e3a9ff2ee4da52311728
Jan <mail@janchristlieb.de>
2018-02-02 15:05:22 +0100

    created a function to seed user into the db

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         factory(\App\Post::class, 20)->create();
         factory(\App\Comment::class,30)->create();
+        factory(\App\User::class,5)->create();
     }
 }
