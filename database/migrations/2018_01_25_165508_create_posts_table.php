9b4681ae4c8dd90b9082536455bb236761c9d72d
Jan <mail@janchristlieb.de>
2018-02-02 15:04:35 +0100

    added a user_id column

--- database/migrations/2018_01_25_165508_create_posts_table.php
+++ database/migrations/2018_01_25_165508_create_posts_table.php
@@ -15,6 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
             $table->timestamps();
             $table->string('title');
             $table->string('slug');
