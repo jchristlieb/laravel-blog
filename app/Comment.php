df55a1d3009e1952a6577c6f1cc907910ff76529
Jan <mail@janchristlieb.de>
2018-02-02 15:03:04 +0100

    a comment belongs to a user

--- app/Comment.php
+++ app/Comment.php
@@ -13,4 +13,12 @@ class Comment extends Model
         return $this->belongsTo(Post::class);
 
     }
+
+    public function user()
+
+    {
+
+        return $this->belongsTo(User::class); // $comment->user->name
+
+    }
 }
