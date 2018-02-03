83191b22be5da0acfe6b1f66bd199ab1e6512cf0
Jan <mail@janchristlieb.de>
2018-02-03 13:48:02 +0100

    adjusted the views for comments

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,7 +9,8 @@
                 <h2 class="mb-0">
                     <a class="text-dark" href="/blog/{{ $post->slug }}">{{ $post->title }}</a>
                 </h2>
-                <div class="mb-1 text-muted">{{ $post->created_at->toFormattedDateString()  }}</div>
+                <p class="mb-1 text-muted">{{ $post->created_at->toFormattedDateString()  }} by {{ $post->user->name
+                }}</p>
                 <p class="card-text mb-auto">{{ $post->abstract }}</p>
                 <a href="/blog/{{ $post->slug }}">Continue reading</a>
             </div>
