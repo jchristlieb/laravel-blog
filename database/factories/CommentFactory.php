5bd6a3de40c6e11c75a7f17318015ee8d3202e44
Manuel Christlieb <manuel.christlieb@googlemail.com>
2018-02-03 13:45:22 +0100

    Refactor Seeder and add .env.example (#1)
    * add .env.example
    
    * refactor seeder

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Comment::class, function (Faker $faker) {
     return [
-        'body' => $faker->paragraph(rand(1,4), true),
-        'user_id' => $faker->numberBetween(1,3),
-        'post_id' => $faker->numberBetween(1,20),
+        'body' => $faker->paragraph(rand(1, 4), true),
+        'post_id' => $faker->numberBetween(1, 20),
+        'website' => $faker->url,
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        factory(\App\Post::class, 20)->create();
-        factory(\App\Comment::class,30)->create();
-        factory(\App\User::class,5)->create();
+        // Create User
+        $user = factory(\App\User::class)->create([
+            'name' => 'Jan Christlieb',
+            'email' => 'mail@janhristlieb.de',
+            'password' => bcrypt('password')
+        ]);
+        $this->command->info('User created');
+
+        // Create Posts with the generated user as author
+        $posts = factory(\App\Post::class, 20)->create(['user_id' => $user->id]);
+        $this->command->info('Posts created');
+
+        // Create comments for each post
+        $posts->each(function ($post) {
+            factory(\App\Comment::class,rand(0,5))->create([
+                'post_id' => $post->id,
+            ]);
+        });
+        $this->command->info('Comments created');
     }
 }
